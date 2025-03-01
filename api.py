b433c6f75de941742df9725470c06ace050561a6
adrian.horvat <adrian.horvat02@gmail.com>
2025-03-01 14:56:33 +0100

    Fix

--- api.py
+++ api.py
@@ -3,30 +3,41 @@ import pandas as pd
 from fastapi import FastAPI
 from pydantic import BaseModel
 
-# Inicijalizacija FastAPI aplikacije
-api = FastAPI()
+
+app = FastAPI()
 
 # Učitavanje modela
 with open("laptop_price_model.pkl", "rb") as f:
     model = pickle.load(f)
 
+categorical_columns = ["Brand", "Processor", "Storage", "GPU", "Resolution", "Operating_System"]
+
 class LaptopFeatures(BaseModel):
     Brand: str
     Processor: str
     RAM_GB: int
-    Storage: int
+    Storage: str  
     GPU: str
     Screen_Size_inch: float
-    Resolution: str
+    Resolution: str  
     Battery_Life_hours: float
     Weight_kg: float
     Operating_System: str
 
-@api.post("/predict")
+@app.post("/predict")
 def predict(features: LaptopFeatures):
-
+  
     df = pd.DataFrame([features.dict()])
     
-    prediction = model.predict(df)
+    df = pd.get_dummies(df, columns=categorical_columns)
+
+    model_columns = model.feature_names_in_
+    missing_cols = [col for col in model_columns if col not in df.columns]
+    for col in missing_cols:
+        df[col] = 0
+
+    df = df[model_columns]
     
-    return {"predicted_price": prediction[0]}
+    prediction = model.predict(df)
+
+    return {"predicted_price": float(prediction[0])}
