7d7b316d228aa6d32643f29e4f051adfbeebb13e
adrian.horvat <adrian.horvat02@gmail.com>
2025-03-01 16:20:56 +0100

    Use scaler file

--- api.py
+++ api.py
@@ -2,13 +2,15 @@ import pickle
 import pandas as pd
 from fastapi import FastAPI
 from pydantic import BaseModel
-
+from sklearn.preprocessing import MinMaxScaler
 
 app = FastAPI()
 
-# Učitavanje modela
 with open("laptop_price_model.pkl", "rb") as f:
     model = pickle.load(f)
+
+with open("scaler.pkl", "rb") as f:
+    scaler = pickle.load(f)
 
 categorical_columns = ["Brand", "Processor", "Storage", "GPU", "Resolution", "Operating_System"]
 
@@ -26,18 +28,17 @@ class LaptopFeatures(BaseModel):
 
 @app.post("/predict")
 def predict(features: LaptopFeatures):
-  
     df = pd.DataFrame([features.dict()])
-    
+
     df = pd.get_dummies(df, columns=categorical_columns)
 
     model_columns = model.feature_names_in_
-    missing_cols = [col for col in model_columns if col not in df.columns]
-    for col in missing_cols:
-        df[col] = 0
 
-    df = df[model_columns]
-    
+    df = df.reindex(columns=model_columns, fill_value=0)
+
+    numeric_features = ["RAM_GB", "Screen_Size_inch", "Battery_Life_hours", "Weight_kg"]
+    df[numeric_features] = scaler.transform(df[numeric_features])  
+
     prediction = model.predict(df)
 
     return {"predicted_price": float(prediction[0])}
